083a73b9fc1d798bc684be6c50c20b9a99aabc4f
Joshua Crum <joshua.crum@gmail.com>
2019-05-02 15:45:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["Lisp", "C"]}
+		 programmer_hash
 end
 
 def changing_alan
