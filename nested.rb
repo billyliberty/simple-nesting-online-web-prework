8b19d177ca40ef15afcdb295b5fe1925f6d7b77f
Joshua Crum <joshua.crum@gmail.com>
2019-05-02 15:53:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,6 +118,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] = "Assemby"
+		 programmer_hash[:dennis_ritchie][:languages].merge = "Assemby"
 		 programmer_hash
 end
